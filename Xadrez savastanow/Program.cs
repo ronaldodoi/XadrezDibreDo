09beeea99d93afd4bb9ad77c19f6e42dd8dbd061
Arthur Pereira <arthurpolima@gmail.com>
2016-10-03 10:42:57 -0300

    Começando a colocar as peças no tabuleiro

--- Xadrez savastanow/Program.cs
+++ Xadrez savastanow/Program.cs
@@ -1,13 +1,15 @@
 ﻿using System;
 using tabuleiro;
+using xadrez;
 
-namespace Xadrez_savastanow
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
+
+namespace Xadrez_savastanow{
+    class Program{
+        static void Main(string[] args){
             Tabuleiro tab = new Tabuleiro(8, 8);
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab,Cor.Preta), new Posicao(2, 4));
             Tela.imprimirTabuleiro(tab);
             Console.ReadLine();
         }
--- Xadrez savastanow/Tela.cs
+++ Xadrez savastanow/Tela.cs
@@ -1,24 +1,19 @@
 ﻿using System;
 using tabuleiro;
 
-namespace Xadrez_savastanow
-{
-    class Tela
-    {
-        public static void imprimirTabuleiro(Tabuleiro tab)
-        {
-            for (int i=0; i < tab.linhas; i++)
-            {
-                for (int j=0; j<tab.colunas; j++)
-                {
-                    if (tab.peca(i, j) == null)
-                    {
+namespace Xadrez_savastanow{
+    class Tela{
+        public static void imprimirTabuleiro(Tabuleiro tab){
+            for (int i=0; i < tab.linhas; i++){
+                for (int j=0; j<tab.colunas; j++){
+                    if (tab.peca(i, j) == null){
                         Console.Write("- ");
                     }
                     else {
-                        Console.Write(tab.peca(i, j) + " ");
+                        Console.Write(tab.peca(i, j));
+                        Console.Write(" ");
                     }
-                    Console.Write(tab.peca(i, j)   +" ");
+                    
                 }
                 Console.WriteLine();
             }
