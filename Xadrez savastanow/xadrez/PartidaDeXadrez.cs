e0b803cbb6ee2b9e9cf4c140974aa0b83730ff85
ronaldodoi <arthurpolima@gmail.com>
2016-10-31 00:11:12 -0200

    Jogada especial: Promoção

--- Xadrez savastanow/xadrez/PartidaDeXadrez.cs
+++ Xadrez savastanow/xadrez/PartidaDeXadrez.cs
@@ -141,6 +141,21 @@ namespace xadrez
 				desfazMovimento(origem, destino, pecaCapturada);
 				throw new TabuleiroException("Você não pode se colocar em cheque!");
 			}
+
+			Peca p = tab.peca(destino);
+
+			//Jogada Especial Promocao
+
+			if (p is Peao) {
+				if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7)) {
+					p = tab.retirarPeca(destino);
+					pecas.Remove(p);
+					Peca dama = new Dama(tab, p.cor);
+					tab.colocarPeca(dama, destino);
+					pecas.Add(dama);
+				}
+			}
+
 			if (estaEmXeque(adversaria(jogadorAtual)))
 			{
 				xeque = true;
@@ -157,7 +172,6 @@ namespace xadrez
 				turno++;
 				mudaJogador();
 			}
-			Peca p = tab.peca(destino);
 
 			//#Jogada Especial EnPassant
 			if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha + 2))
